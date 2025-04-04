58d9b2eee5a2c7502796cf563a49cac6f7624e77
BT_1N3_14 <example@gmail.com>
2025-04-04 13:02:14 -0400

    did 2nd tilemap

--- src/TileMap.java
+++ src/TileMap.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.nio.Buffer;
 
 public class TileMap {
 
@@ -18,4 +19,30 @@ public class TileMap {
         canvas.dispose();
         return tileMap;
     }
+
+
+
+    public static BufferedImage tileMapMaker(String imagePath,int x, int y)  {
+        BufferedImage image=null;
+        try {
+            image = ImageIO.read(new File(imagePath));
+        } catch (IOException e) {
+            System.out.println("nope");
+        }
+        BufferedImage tileMap;
+        tileMap=new BufferedImage(image.getWidth()*x,image.getHeight()*y,BufferedImage.TYPE_INT_ARGB);
+        Graphics2D canvas = tileMap.createGraphics();
+        for (int i = 0; i < tileMap.getHeight(); i+=image.getHeight()) {
+            for (int j = 0; j < tileMap.getWidth(); j+=image.getWidth()) {
+                canvas.drawImage(image,j,i,null);
+            }
+        }
+        canvas.dispose();
+        return tileMap;
+    }
+
 }
+
+
+
+
