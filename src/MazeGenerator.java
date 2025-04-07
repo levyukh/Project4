154d88e41461ab2abdc0b1478ae6321c1cc8f16c
Lev Yukhananov <levy@nycstudents.net>
2025-04-07 01:12:50 -0400

    absolute cinemaaa

--- src/MazeGenerator.java
+++ src/MazeGenerator.java
@@ -1,26 +1,16 @@
-import java.util.Arrays;
 
-
-
-   import java.util.Arrays;
 import java.util.Random;
-
     public class MazeGenerator {
-
         static Random rand = new Random();
         static int[] dx = {0, 1, 0, -1}; // Directions: top, right, bottom, left
         static int[] dy = {-1, 0, 1, 0};
         static int[] opposite = {2, 3, 0, 1}; // Opposites: 0<->2, 1<->3
-
         public static boolean[][][] maze(int width, int height) {
             boolean[][][] rooms = new boolean[width][height][4]; // Each cell has 4 exits
             boolean[][] visited = new boolean[width][height];
-
-
             generate(0, 0, rooms, visited, width, height);
             return rooms;
         }
-
         private static void generate(int x, int y, boolean[][][] rooms, boolean[][] visited, int width, int height) {
             visited[x][y] = true;
 
@@ -41,7 +31,6 @@ import java.util.Random;
                 }
             }
         }
-
         private static void shuffle(int[] array) {
             for (int i = array.length - 1; i > 0; i--) {
                 int j = rand.nextInt(i + 1);
@@ -50,8 +39,5 @@ import java.util.Random;
                 array[j] = tmp;
             }
         }
-
-
-
     }
 
