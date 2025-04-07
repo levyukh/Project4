6bbf31c969f5865b2c80ed0defbe1bf55b1ab7eb
Lev Yukhananov <levy@nycstudents.net>
2025-04-06 23:02:58 -0400

    maze

--- src/Display.java
+++ src/Display.java
@@ -9,29 +9,18 @@ import java.awt.event.MouseListener;
 public class Display extends JPanel implements KeyListener, MouseListener {
 
 
-    Room room=new Room("Sprites/floor.png",25,20,false,true,false,true);
-    Room room2=new Room("Sprites/floor.png",25,20,true,true,true,true);
-    CollidableEntity gf=new CollidableEntity(room2,130,120,300,120,"Sprites/wall.png");
-    Room room3=new Room("Sprites/floor.png",25,20,true,true,true,true);
-    CollidableEntity gff=new CollidableEntity(room3,130,120,300,120,"Sprites/image.png");
-    CollidableEntity f=new CollidableEntity(room3,130,120,400,180,"Sprites/image.png");
-    CollidableEntity gfff=new CollidableEntity(room3,130,20,500,220,"Sprites/image.png");
-    CollidableEntity ff=new CollidableEntity(room3,130,120,700,190,"Sprites/image.png");
-    Room room4=new Room("Sprites/floor.png",25,20,true,true,true,true);
+  Room[][] rooms=new Room[10][10];
 
 
 
-    Room[][] x=
-            {{room, room2},
-                    {room3, room4}};
-    Player player=new Player(x,100,100,70,70,"Sprites/image.png",3,500,2,0,0);
+    Player player=new Player(rooms,100,100,70,70,"Sprites/image.png",3,500,2,0,0);
     Thread gameLoop=new Thread(new GameLoop(player,this));
 
     public Display(int width, int height, String title){
-        new OverworldItem(room,50,50,400,520,new Item());
-        new OverworldItem(room,50,50,480,520,new Item());
-        new LivingEntity(room,100,30,500,520,"Sprites/floor.png",3,200);
-        new Enemy(room2,50,30,400,320,"Sprites/red.png",3,200,1);
+        boolean[][][] maze = MazeGenerator.maze(rooms.length, rooms[0].length);
+        for (Room[] hallway:rooms) {
+            //for(Room room:hallway) room=new Room()
+        }
         JFrame frame = new JFrame(title);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(width, height);
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args)  {
-
+        MazeGenerator.maze(10,10);
         Display x=new Display(880, 640, "hi");
 
     }
